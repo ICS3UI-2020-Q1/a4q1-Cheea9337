fde5e9f573a89fbd13387e8e6de01a61f8af2de0
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 14:27:54 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * count down until the number the user inputs
+ * @author Aidan Cheesmond
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // declare the variable
+    int downTo;
+    do{
+      System.out.println("What number would you like to count down to?");
+      downTo = input.nextInt();
+    }while(downTo <= 0);
+
+    // create a variable to keep track of the number
+    int count = 100;
+    // using a loop to count down to user's number
+    while(count >= downTo){
+      System.out.println(count);
+      // decrease the value of the count by five
+      count = count - 5;
+    }
+
     
   }
 }
